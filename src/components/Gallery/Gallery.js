60d7c89d696a811836aea294ebeacb7f501ecae6
Nabin Karki <nabin@Nabins-MBP.lan>
2019-03-23 18:01:56 +1100

    Mobile Device image-show issue prod

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,8 +1,8 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
 import './Gallery.css'
 
-import {connect} from "react-redux";
+import { connect } from "react-redux";
 
 import * as _ from '../../store/actions/actionCreator'
 import Loading from "../Loading/Loading";
@@ -27,51 +27,50 @@ class Gallery extends Component {
     };
     render() {
         const images = this.props.images && this.props.images.images;
-        const {photoIndex, isOpen} = this.state;
+        const { photoIndex, isOpen } = this.state;
         let cardImage = null;
-        if (this.props.images.isLoading) {
-            cardImage = <Loading/>;
-        } else {
-            cardImage = (
-                <div className={'row d-flex d-flex justify-content-center'}>
-                    {images.map((image, index) => {
-                        return (
-                            <div key={image.pk} className='col-sm-6 col-md-2 col-lg-2 m-1 p-0 img-responsive'>
-                                <div>                
-                                    <div>
-                                        <img onClick={() => {
-                                            this.pkAndIsOpenHandler(index);
-                                            this.props.show();
-                                        }} id={'cardImage'} src={image.image} title={image.title} alt=""/>
-                                        {/* <h3 id="image-text" className="text-center">{image.title}</h3> */}
-                                    </div>
+        cardImage = (
+            <div className={'row d-flex d-flex justify-content-center'}>
+                {images.map((image, index) => {
+                    return (
+                        <div key={image.pk} className='col-sm-6 col-md-2 col-lg-2 m-1 p-0 img-responsive'>
+                            <div>
+                                <div>
+                                    <img onClick={() => {
+                                        this.pkAndIsOpenHandler(index);
+                                        this.props.show();
+                                    }} id={'cardImage'} src={image.image} title={image.title} alt="" />
+                                    {/* <h3 id="image-text" className="text-center">{image.title}</h3> */}
                                 </div>
-                                {isOpen && <Lightbox
-                                    mainSrc={images[photoIndex].image}
-                                    imagePadding ={50}
-                                    nextSrc={images[(photoIndex + 1) % images.length].image}
-                                    imageTitle={images[photoIndex].title}
-                                    prevSrc={images[(photoIndex + images.length - 1) % images.length].image}
-                                    onCloseRequest={() => {
-                                        this.setState({isOpen: false});
-                                        this.props.show()
-                                    }}
-                                    onMovePrevRequest={() => {
-                                        this.setState({photoIndex: (photoIndex + images.length - 1) % images.length})
-                                    }}
-                                    onMoveNextRequest={() => {
-                                        this.setState({photoIndex: (photoIndex + 1) % images.length})
-                                    }}
-                                />}
                             </div>
-                        )
-                    })}
-                </div>
-            )
-        }
+                            {isOpen && <Lightbox
+                                mainSrc={images[photoIndex].image}
+                                imagePadding={50}
+                                nextSrc={images[(photoIndex + 1) % images.length].image}
+                                imageTitle={images[photoIndex].title}
+                                prevSrc={images[(photoIndex + images.length - 1) % images.length].image}
+                                onCloseRequest={() => {
+                                    this.setState({ isOpen: false });
+                                    this.props.show()
+                                }}
+                                onMovePrevRequest={() => {
+                                    this.setState({ photoIndex: (photoIndex + images.length - 1) % images.length })
+                                }}
+                                onMoveNextRequest={() => {
+                                    this.setState({ photoIndex: (photoIndex + 1) % images.length })
+                                }}
+                            />}
+                        </div>
+                    )
+                })}
+            </div>
+        )
+        // if (this.props.images.isLoading) {
+        //     cardImage = <Loading />;
+        // }
         return (
             <div id='Gallery'>
-                {cardImage}
+                {this.props.images.isLoading? <Loading/>:cardImage}
             </div>
 
 
